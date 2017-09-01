2ec237a6a3705bef802ff9909e3d035cfac1c2c8
qyl <yuelinshi@qq.com>
2017-09-01 12:56:45 +0800

    表单

--- Flask练习/hello.py
+++ Flask练习/hello.py
@@ -3,15 +3,19 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+#@app.route('/')
+#def index():
+    #return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -25,7 +29,22 @@ def page_not_found(e):
 def internal_server_error(e):
 	return render_template('500.html'), 500
 
+#定义一个表单类，参数validators指定验证函数Required()，Submit提交按钮
+class NameForm(Form):
+	name = StringField('你叫什么名字?', validators=[Required()])
+	submit = SubmitField('确认')
 
+#表单的路由方法，validate_on_submit()调用name上的Required()，为True用户输入的就通过
+#data属性获取，然后清空表单字段form.name.data；最后(为False直接)渲染表单模块,	
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name, 
+							current_time=datetime.utcnow())
 
 if __name__ == '__main__':
     manager.run()
