249efc1eb9c018346e26c302d491ecb43b03085d
qyl <yuelinshi@qq.com>
2017-09-03 09:30:27 +0800

    .

--- Flask练习/hello.py
+++ Flask练习/hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -6,20 +7,21 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+			'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-db = SQLALchemy(app)
+db = SQLAlchemy(app)
 
 #@app.route('/')
 #def index():
@@ -61,6 +63,7 @@ class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role', lazy='dynamic')
 	
 	def __repr__(self):
 		return '<Role %r>' % self.name
@@ -69,6 +72,7 @@ class User(db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), unique=True, index=True)
+	rold_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 	
 	def __repr__(self):
 		return '<User %r>' % self.username
